636a0e6b92c674302b177a695a00dc3dddb5ad3e
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-01-22 20:13:29 +0100

    More about addressing.

--- simple_example/simple_example2.s
+++ simple_example/simple_example2.s
@@ -1,13 +1,19 @@
-
 // Different addressing types 
 .global _start
 _start:
 
-    MOV R0, #5 // immediate addressing (moving a immediate/constant into a register)
-    MOV R1, R0 // register direct addressing (moving data from register to register)
+//    MOV R0, #5 // immediate addressing (moving a immediate/constant into a register)
+//    MOV R1, R0 // register direct addressing (moving data from register to register)
 
     // LDR = load data from stack into registers
-    LDR R2, =list // Loads the first value of list into register R2, direct addressing
+    LDR R0, =list // Loads the first value of list into register R2, direct addressing. CPUlater look into memory
+	
+	// Retrieve data from list. We are moving from the stack into the register
+	// Load into register R1 the value that exists at the address of R0
+	LDR R1, [R0]
+	// Takes the value/address from R0 adds 4 and retrieves the value in that memory location
+	LDR R2,[R0, #4]
+	
 
 
 // get data on the stack
